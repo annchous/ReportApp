31c681fef5549908e2d8e51a35daa5f52b2a044f
Annchous <57408376+annchous@users.noreply.github.com>
2020-12-14 15:47:24 +0300

    feat: TaskChangeDto added

---
+++ ReportApp.Core/DTO/TaskChangeDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ReportApp.Core.DTO
+{
+    public class TaskChangeDto
+    {
+        public Int32 Id { get; set; }
+        public DateTime? Date { get; set; }
+        public String Comment { get; set; }
+    }
+}
