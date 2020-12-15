50ece5b4401dce32464e8833801dfb310b011a1f
Annchous <57408376+annchous@users.noreply.github.com>
2020-12-15 14:20:45 +0300

    feat: db connection string added

--- ReportApp.Server/Startup.cs
+++ ReportApp.Server/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReportApp.DAL.Context;
 
 namespace ReportApp.Server
 {
@@ -23,6 +25,8 @@ namespace ReportApp.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ReportAppContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
