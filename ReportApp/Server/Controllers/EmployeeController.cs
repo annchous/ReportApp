90e7970fabcd4ffeb3aa688b540d20f113fae566
Annchous <57408376+annchous@users.noreply.github.com>
2020-12-17 19:17:34 +0300

    feat: controllers added

---
+++ ReportApp/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReportApp.Core.DTO;
+using ReportApp.Core.Interfaces;
+
+namespace ReportApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("get-all")]
+        public async Task<ActionResult<List<EmployeeDto>>> GetAllEmployeesAsync()
+        {
+            var result = await _employeeService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<EmployeeDto>> GetEmployeeByIdAsync(Int32 id)
+        {
+            var result = await _employeeService.GetEmployeeAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateEmployeeAsync(EmployeeDto employee)
+        {
+            await _employeeService.CreateEmployeeAsync(employee);
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateEmployeeAsync(EmployeeDto employee)
+        {
+            await _employeeService.UpdateEmployeeAsync(employee);
+            return Ok();
+        }
+    }
+}
---
+++ ReportApp/Server/Controllers/ReportController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReportApp.Core.DTO;
+using ReportApp.Core.Interfaces;
+
+namespace ReportApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpGet("get-all")]
+        public async Task<ActionResult<List<ReportDto>>> GetAllReportsAsync()
+        {
+            var result = await _reportService.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ReportDto>> GetReportByIdAsync(Int32 id)
+        {
+            var result = await _reportService.GetReportAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateReportAsync(ReportDto report)
+        {
+            await _reportService.CreateReportAsync(report);
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateReportAsync(ReportDto report)
+        {
+            await _reportService.UpdateReportAsync(report);
+            return Ok();
+        }
+    }
+}
